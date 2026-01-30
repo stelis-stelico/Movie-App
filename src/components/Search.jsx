6c2e8abc6b711deb616bc6537f30a46ca4e28f50
stelis-stelico <stelisstelico@gmail.com>
2026-01-30 17:09:26 +0100

    Initial commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+const Search = ({searchTerm, setSearchTerm}) => {
+  return (
+    <div className="search">
+        <div>
+            <img src="search.svg" alt="search" />
+
+            <input type="text" placeholder="search through thousands of movies" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} />
+        </div>
+        
+    </div>
+  )
+}
+
+export default Search
